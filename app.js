04e2c7a259d08cc0ccafef8acc2cc92b669bd26f
AGKL01 <Ahmadghassankle@gmail.com>
2025-01-21 11:29:05 +0300

    e

--- app.js
+++ app.js
@@ -102,6 +102,10 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
+app.get('/index', (req, res) => {
+  res.render('index');
+});
+
 app.get('/home', (req, res) => {
   res.render('home');
 });
